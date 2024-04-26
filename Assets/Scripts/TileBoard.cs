7f55b069c5a9323dc038de2bdede10f4100e488b
zakumann <samuel742able@gmail.com>
2024-04-26 15:01:56 +0900

    Add animation, GameOver and Retry

--- Assets/Scripts/TileBoard.cs
+++ Assets/Scripts/TileBoard.cs
@@ -1,14 +1,15 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TileBoard : MonoBehaviour
 {
+    public GameManager gameManager;
     public Tile tilePrefab;
     public TileState[] tileStates;
 
     private TileGrid grid;
     private List<Tile> tiles;
-
     private bool waiting;
 
     private void Awake()
@@ -17,13 +18,22 @@ public class TileBoard : MonoBehaviour
         tiles = new List<Tile>(16);
     }
 
-    private void Start()
+    public void ClearBoard()
     {
-        CreateTile();
-        CreateTile();
+        foreach(var cell in grid.cells)
+        {
+            cell.tile = null;
+        }
+
+        foreach (var tile in tiles)
+        {
+            Destroy(tile.gameObject);
+        }
+
+        tiles.Clear();
     }
 
-    private void CreateTile()
+    public void CreateTile()
     {
         Tile tile = Instantiate(tilePrefab, grid.transform);
         tile.SetState(tileStates[0], 2);
@@ -33,26 +43,31 @@ public class TileBoard : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (!waiting)
         {
-            MoveTiles(Vector2Int.up, 0, 1, 1, 1);
-        }
-        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            MoveTiles(Vector2Int.down, 0, 1, grid.height - 2, -1);
-        }
-        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            MoveTiles(Vector2Int.left, 1, 1, 0, 1);
-        } 
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            MoveTiles(Vector2Int.right, grid.width - 2, -1, 0, 1);
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                MoveTiles(Vector2Int.up, 0, 1, 1, 1);
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                MoveTiles(Vector2Int.down, 0, 1, grid.height - 2, -1);
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                MoveTiles(Vector2Int.left, 1, 1, 0, 1);
+            } 
+            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                MoveTiles(Vector2Int.right, grid.width - 2, -1, 0, 1);
+            }
         }
     }
 
     private void MoveTiles(Vector2Int direction, int startX, int incrementX, int startY, int incrementY)
     {
+        bool changed = false;
+
         for(int x = startX; x >= 0 && x < grid.width; x += incrementX)
         {
             for (int y = startY; y >= 0 && y < grid.height; y += incrementY)
@@ -61,13 +76,19 @@ public class TileBoard : MonoBehaviour
 
                 if(cell.occupied)
                 {
-                    MoveTile(cell.tile, direction);
+                    changed = MoveTile(cell.tile, direction);
                 }
             }
         }
+
+        if (changed)
+        {
+            StartCoroutine(WaitForChanges());
+        }
+
     }
 
-    private void MoveTile(Tile tile, Vector2Int direction)
+    private bool MoveTile(Tile tile, Vector2Int direction)
     {
         TileCell newCell = null;
         TileCell adjacent = grid.GetAdjacentCell(tile.cell, direction);
@@ -76,7 +97,13 @@ public class TileBoard : MonoBehaviour
         {
             if (adjacent.occupied)
             {
-                // Todo: merging
+                // merging
+                if (CanMerge(tile, adjacent.tile))
+                {
+                    Merge(tile, adjacent.tile);
+                    return true;
+                }
+
                 break;
             }
 
@@ -87,7 +114,104 @@ public class TileBoard : MonoBehaviour
         if (newCell != null)
         {
             tile.MoveTo(newCell);
+            return true;
         }
+
+        return false;
+    }
+
+    // Merging Tiles
+    private bool CanMerge(Tile a, Tile b)
+    {
+        return a.number == b.number && !b.locked;
+    }
+
+    private void Merge(Tile a, Tile b)
+    {
+        tiles.Remove(a);
+        a.Merge(b.cell);
+
+        int index = Mathf.Clamp(IndexOf(b.state) + 1, 0, tileStates.Length - 1);
+        int number = b.number * 2;
+
+        b.SetState(tileStates[index], number);
+    }
+
+    private int IndexOf(TileState state)
+    {
+        for (int i = 0; i < tileStates.Length; i++)
+        {
+            if (state == tileStates[i])
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private IEnumerator WaitForChanges()
+    {
+        waiting = true;
+
+        yield return new WaitForSeconds(0.1f);
+
+        waiting = false;
+        // Todo: create new tile
+
+        foreach (var tile in tiles)
+        {
+            tile.locked = false;
+        }
+
+        if (tiles.Count != grid.size)
+        {
+            CreateTile();
+        }
+        
+        // Todo: check for game over
+        if (CheckForGameOver())
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    private bool CheckForGameOver()
+    {
+        if (tiles.Count != grid.size)
+        {
+            return false;
+        }
+
+        foreach (var tile in tiles)
+        {
+            TileCell up = grid.GetAdjacentCell(tile.cell, Vector2Int.up);
+            TileCell down = grid.GetAdjacentCell(tile.cell, Vector2Int.down);
+            TileCell left = grid.GetAdjacentCell(tile.cell, Vector2Int.left);
+            TileCell right = grid.GetAdjacentCell(tile.cell, Vector2Int.right);
+
+            if(up != null && CanMerge(tile, up.tile))
+            {
+                return false;
+            }
+
+            if (down != null && CanMerge(tile, down.tile))
+            {
+                return false;
+            }
+
+            if (left != null && CanMerge(tile, left.tile))
+            {
+                return false;
+            }
+
+            if (right != null && CanMerge(tile, right.tile))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
